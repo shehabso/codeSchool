3a4586fdb0bc1e4b6771df538a14e743b3ade48f
shehab tarek <shehab162519@gmail.com>
2024-11-01 13:45:45 +0200

    finish and compile variables

---
+++ Rust/variable/src/main.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code, unused_variables, unused_assignments)]
+mod variable_1;
+mod variable_2;
+mod variable_3;
+mod variable_4;
+mod variable_5;
+mod variable_6;
+mod variable_7;
+mod variable_8;
+
+fn main(){}
---
+++ Rust/variable/target/debug/deps/variable.d
@@ -0,0 +1,13 @@
+C:\Users\shehab_tarek\Desktop\github_repo\codeSchool\Rust\variable\target\debug\deps\variable.exe: src/main.rs src\variable_1.rs src\variable_2.rs src\variable_3.rs src\variable_4.rs src\variable_5.rs src\variable_6.rs src\variable_7.rs src\variable_8.rs
+
+C:\Users\shehab_tarek\Desktop\github_repo\codeSchool\Rust\variable\target\debug\deps\variable.d: src/main.rs src\variable_1.rs src\variable_2.rs src\variable_3.rs src\variable_4.rs src\variable_5.rs src\variable_6.rs src\variable_7.rs src\variable_8.rs
+
+src/main.rs:
+src\variable_1.rs:
+src\variable_2.rs:
+src\variable_3.rs:
+src\variable_4.rs:
+src\variable_5.rs:
+src\variable_6.rs:
+src\variable_7.rs:
+src\variable_8.rs:
